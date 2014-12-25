8d3f6beaf55d371491c0ec20e042581787fd35bc
Justin Campbell <jtcmh13@gmail.com>
2014-12-25 14:04:41 -0500

    Objectification of suggest_zip.php with comments
    mysql_connect parameters moved to constants
    mysql_select_db parameters moved to constants
    
    Comments added

--- php/search.php
+++ php/search.php
@@ -1,9 +1,8 @@
-<?php 
+<?php  
+	
+	
 
-	// echo $_REQUEST['zipsearch'];
-
-	// Scratchpad 
-
+	// Select first result from search from airports table after ordering from shortest to longest distance
 	$sql = "SELECT TOP 1 name, latitude, logitude";
 	$sql .= " FROM airports";
 	$sql .= " ORDER BY";
@@ -16,37 +15,5 @@
 
 	// Difference in longitude, squared
 	$sql .= " (longitude - [users destination longitude]) * (longitude - [users destination longitude])";
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// Select the first row from table table airports (now varaible a)
-	// Order by smallest to largest distance squared
-
-	// CALCULATION
-	// (latitude) = loop value
-	// 60.234 = latitude of users selected destination
-
-	// Description
-		// Using the distance formula, this SQL call calculated the distance between the user's distination and the nearest airport to return the cloests aiport
-		// For this call to work I need the users desttination airport
-
-
-
 
 ?>
--- php/suggest_zip.php
+++ php/suggest_zip.php
@@ -3,14 +3,45 @@
 if ( !isset($_REQUEST['term']) )
     exit;
 
-$dblink = mysql_connect('localhost', 'root', 'jman22355') or die( mysql_error() );
-mysql_select_db('bpa2014');
+///////////////
+// Constants //
+///////////////
 
-$rs = mysql_query('select zip, city, state, latitude, longitude from zips where zip like "'. mysql_real_escape_string($_REQUEST['term']) .'%" order by zip asc limit 0,10', $dblink);
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "jman22355");
+define("DB_NAME", "bpa2014");
 
+// Establish Connection to Database
+$dblink = mysql_connect(DB_HOST, DB_USER, DB_PASS) or die(mysql_error());
+
+// Select Database
+mysql_select_db(DB_NAME);
+
+// Define Record Set $rs
+//
+// Query the database
+// SELECT columns zip, city, state, latitude, and longitude
+// FROM table zips
+// WHERE zip is similar (LIKE) the user's input value
+//
+// mysql_real_escape_string(); <-- Formats strings to prevent SQL injections 
+
+$rs = mysql_query('SELECT zip, city, state, latitude, longitude FROM zips WHERE zip LIKE "'. mysql_real_escape_string($_REQUEST['term']) .'%" ORDER BY zip ASC LIMIT 0,10', $dblink);
+
+
+// Define empty array to be populated with results from search and sent to user as populated list
 $data = array();
+
+// Conditional: If there are results to return, continue
 if ( $rs && mysql_num_rows($rs) )
 {
+	// mysql_fetch_array($rs, MYSQL_ASSOC); <-- Searches database for zip code similar to user's input and returns values as associative array
+	// 											for key => value, key = column title and value = row value
+	// 											
+	// While $row is populated from associative array
+	// 		labels in the array will become: ZIP - CITY, STATE
+	// 		example: 47374 - Richmond, Indiana
     while( $row = mysql_fetch_array($rs, MYSQL_ASSOC) )
     {
         $data[] = array(
@@ -20,7 +51,10 @@ if ( $rs && mysql_num_rows($rs) )
     }
 }
 
+// Print out $data in json for jQuery usage
 echo json_encode($data);
+
+// Clear PHP buffer and force send information to browser
 flush();
 
 ?>
